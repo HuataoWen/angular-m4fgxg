bb1702760727829b61205a3adfe333707c98819e
HuataoWen <huataowe@usc.edu>
2021-08-14 00:52:05 -0700

    1 file modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -7,7 +7,10 @@ import { CartService } from '../cart.service';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
-  constructor() {}
+
+  constructor(
+    private cartService: CartService
+    ) {}
 
   ngOnInit() {}
 }
